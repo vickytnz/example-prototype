ff34d2ffdfa339e212adf5670eb9b186022b5f72
vickytnz <vicky.teinaki@gmail.com>
2024-03-05 01:26:16 +0000

    Second version

--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,8 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+addFilter('monthToWords', function (month) {
+      var monthsList = ["January", "February" , "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      var words = monthsList[month - 1];
+      return words;
+  })
--- app/routes.js
+++ app/routes.js
@@ -6,5 +6,37 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
 // Add your routes here
+
+
+
+// GET FOLDER NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+  res.locals.currentURL = req.originalUrl; //current screen
+  res.locals.prevURL = req.get('Referrer'); // previous screen
+  req.folder = req.originalUrl.split('/')[1]; //folder, e.g. 'current'
+  req.subfolder = req.originalUrl.split('/')[2]; //sub-folder e.g. 'service'
+  res.locals.folder = req.folder; // what folder the url is
+  res.locals.subfolder = req.subfolder; // what subfolder the URL is in
+console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
+  console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );
+  next();
+});
+
+// log stuff 
+
+router.use((req, res, next) => {
+
+      const log = {
+        method: req.method,
+        url: req.originalUrl,
+        data: req.session.data
+      }
+      console.log(JSON.stringify(log, null, 2))
+
+    next()
+  })
+
 router.use('/base', require('./views/base/\_routes'));
+router.use('/final', require('./views/final/\_routes'));
